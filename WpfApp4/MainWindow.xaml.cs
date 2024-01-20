5120946d6a0f1d80c8ffa7dcd5707887b7ec60bd
Nikita <warface498nekit@mail.ru>
2024-01-20 21:19:49 +0300

    Добавлена кнопка с информацией об авторе изменения

--- WpfApp4/MainWindow.xaml.cs
+++ WpfApp4/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,11 @@ namespace WpfApp4
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "Текст изменен при нажатии кнопки!";
         }
     }
 }
